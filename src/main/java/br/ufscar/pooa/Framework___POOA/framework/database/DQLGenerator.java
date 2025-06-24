a8d9c8b61ce7ca15d2206090ef63b7ddbf5ef060
Eduardo Malagutti <eduardo.malagutti@catijr.com.br>
2025-06-24 15:31:25 -0300

    style: fix identation

--- src/main/java/br/ufscar/pooa/Framework___POOA/framework/database/DQLGenerator.java
+++ src/main/java/br/ufscar/pooa/Framework___POOA/framework/database/DQLGenerator.java
@@ -17,54 +17,57 @@ public class DQLGenerator {
                 VALUES (%s)
                 """, tableName, columnNames, questionMarks);
     }
-public String generateSelectByIdSQL(String tableName, List<Field> columns, Field idField) {
-    StringBuilder sql = new StringBuilder("SELECT ");
-    
-    // Adicionar todas as colunas
-    for (int i = 0; i < columns.size(); i++) {
-        Field field = columns.get(i);
-        Column columnAnnotation = field.getAnnotation(Column.class);
-        sql.append(columnAnnotation.name());
-        
-        if (i < columns.size() - 1) {
-            sql.append(", ");
+
+    public String generateSelectByIdSQL(String tableName, List<Field> columns, Field idField) {
+        StringBuilder sql = new StringBuilder("SELECT ");
+
+        // Adicionar todas as colunas
+        for (int i = 0; i < columns.size(); i++) {
+            Field field = columns.get(i);
+            Column columnAnnotation = field.getAnnotation(Column.class);
+            sql.append(columnAnnotation.name());
+
+            if (i < columns.size() - 1) {
+                sql.append(", ");
+            }
         }
+
+        sql.append(" FROM ").append(tableName);
+        sql.append(" WHERE ");
+
+        Column idColumnAnnotation = idField.getAnnotation(Column.class);
+        sql.append(idColumnAnnotation.name()).append(" = ?");
+
+        return sql.toString();
     }
-    
-    sql.append(" FROM ").append(tableName);
-    sql.append(" WHERE ");
-    
-    Column idColumnAnnotation = idField.getAnnotation(Column.class);
-    sql.append(idColumnAnnotation.name()).append(" = ?");
-    
-    return sql.toString();
-}
-public String generateSelectAllSQL(String tableName, List<Field> columns) {
-    StringBuilder sql = new StringBuilder("SELECT ");
-    
-    // Adicionar todas as colunas
-    for (int i = 0; i < columns.size(); i++) {
-        Field field = columns.get(i);
-        Column columnAnnotation = field.getAnnotation(Column.class);
-        sql.append(columnAnnotation.name());
-        
-        if (i < columns.size() - 1) {
-            sql.append(", ");
+
+    public String generateSelectAllSQL(String tableName, List<Field> columns) {
+        StringBuilder sql = new StringBuilder("SELECT ");
+
+        // Adicionar todas as colunas
+        for (int i = 0; i < columns.size(); i++) {
+            Field field = columns.get(i);
+            Column columnAnnotation = field.getAnnotation(Column.class);
+            sql.append(columnAnnotation.name());
+
+            if (i < columns.size() - 1) {
+                sql.append(", ");
+            }
         }
+
+        sql.append(" FROM ").append(tableName);
+
+        return sql.toString();
     }
-    
-    sql.append(" FROM ").append(tableName);
-    
-    return sql.toString();
-}
-public String generateExistsSQL(String tableName, Field idField) {
-    StringBuilder sql = new StringBuilder("SELECT COUNT(*) FROM ");
-    sql.append(tableName);
-    sql.append(" WHERE ");
-    
-    Column idColumnAnnotation = idField.getAnnotation(Column.class);
-    sql.append(idColumnAnnotation.name()).append(" = ?");
-    
-    return sql.toString();
-}
+
+    public String generateExistsSQL(String tableName, Field idField) {
+        StringBuilder sql = new StringBuilder("SELECT COUNT(*) FROM ");
+        sql.append(tableName);
+        sql.append(" WHERE ");
+
+        Column idColumnAnnotation = idField.getAnnotation(Column.class);
+        sql.append(idColumnAnnotation.name()).append(" = ?");
+
+        return sql.toString();
+    }
 }
